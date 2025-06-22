72caa474b553b729c423f70fa5e1c04ddf802e8a
jonsnow966 <jonsnow966usk@gmail.com>
2025-06-22 06:31:28 +0500

    3.3 commit - Minor Adjustments - 22-06-2025

--- src/Components/HeaderPrimary/HeaderPrimary.tsx
+++ src/Components/HeaderPrimary/HeaderPrimary.tsx
@@ -4,7 +4,7 @@ import SearchBar from "./SearchBar/SearchBar"
 function HeaderPrimary() {
   return (
     <div className="bg-[#f8f8f8] w-full h-[110px] transition-all duration-500 ease-in-out
-    md:h-[190px] px-5 md:pl-[44px] md:pr-8 md:pb-5 fixed top-0 left-0 right-0 z-50 flex flex-col items-center">
+    md:h-[190px] pl-5 pr-[18px] md:px-5 md:pl-[44px] md:pr-8 md:pb-5 fixed top-0 left-0 right-0 z-50 flex flex-col items-center">
         <HeaderSecondary/>
         <SearchBar/>
     </div>  
